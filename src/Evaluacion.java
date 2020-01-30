230bfa3764a198f92c04faf22d36c05a8dbb8388
josecarlosLH <60482743+josecarlosLH@users.noreply.github.com>
2020-01-30 19:49:06 +0100

    Update Evaluacion.java

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -13,7 +13,6 @@ public class Evaluacion {
     public static void main(String[] args) {
         
         System.out.println("Si tu nombre está aquí tienes un punto adicional");
-        System.out.println("José Carlos");
     }
     
 }
