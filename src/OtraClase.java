3e41cc2de135a315cd2f59a39d97d041151aa903
idbis <idbis@idbis-SATELLITE-PRO-A50-C>
2020-01-30 13:10:37 +0100

    Tras Merge Rama1

--- src/OtraClase.java
+++ src/OtraClase.java
@@ -11,9 +11,7 @@
 public class OtraClase {
     
     public static void main(String[] args){
-        alsjhfdaldsf
-                +asdfñasdkfm
-                asñfdjsañldfm
+        System.out.println("Arreglado");
     } 
     
 }
