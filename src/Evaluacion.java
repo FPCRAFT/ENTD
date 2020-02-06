1be44bfc3b95c864e48fb4acada4a8c1b7f8328a
Linkeriyo <linkeriyo@gmail.com>
2020-02-06 19:03:42 +0100

    Juan Carlos se añade

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -13,6 +13,7 @@ public class Evaluacion {
     public static void main(String[] args) {
         
         System.out.println("Si tu nombre está aquí tienes un punto adicional");
+        System.out.println("Juan Carlos Martín López");
     }
     
 }
