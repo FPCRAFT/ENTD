5f9c382cfdca2e1e75c5b596cfe09997e66b74c5
inaki_57htt38 <inaki_57htt38@LAPTOP-UUHJFKGV>
2020-02-06 19:55:04 +0100

    iñaki

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -25,7 +25,7 @@ public class Evaluacion {
         System.out.println("Maria Dolores");
         System.out.println("Cristian ñ-ñ");
         System.out.println("ALEX HAAKE");
-        System.out.println("Inaki");
+        System.out.println("Iñaki");
         
         
     }
