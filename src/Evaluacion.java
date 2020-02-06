c8aeff87329ad8c0bf47783645a35a253e85ed49
Ivan <ivanmolinagamarra1@gmail.com>
2020-02-06 19:07:51 +0100

    Clase 6 de febrero--

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -1,9 +1,8 @@
 public class Evaluacion {
     
     public static void main(String[] args) {
-        
-        System.out.println("Si tu nombre está aquí tienes un punto adicional");
-        System.out.println("Isabel :)");
+       
+        System.out.println("Ivan :)");
     }
     
 }
