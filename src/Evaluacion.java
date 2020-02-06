0c0c596e51e0596cb41b7fb24ca1f500d1f63efd
USUARIO <USUARIO@DESKTOP-E86HSQB>
2020-02-06 19:23:33 +0100

    INICIO

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -8,7 +8,7 @@ public class Evaluacion {
         System.out.println("Jose Antonio Duarte Perez");
         System.out.println("Ivan :)");
         System.out.println("José Carlos");
-
+        System.out.println("Manuéh");
         System.out.println("Rafael Escudero Ortega");
         
         
